a675bc9d6b2942f8999402f529c7b6842d15b7af
dioomurilo13 <murilodiogo4@gmail.com>
2026-07-09 15:24:07 -0400

    chore: scaffold do app Android com Gradle wrapper

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Build de nivel raiz. Os plugins sao declarados aqui e aplicados no modulo :app.
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
